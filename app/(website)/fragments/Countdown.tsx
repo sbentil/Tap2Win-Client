096204c8ec54194f4af16d1b97d79666ec6e2fb0
qbentil <bentilshadrack72@gmail.com>
2025-04-24 12:19:56 +0100

    Close purchase

--- app/(website)/fragments/Countdown.tsx
+++ app/(website)/fragments/Countdown.tsx
@@ -1,5 +1,6 @@
-import { MessageQuestion } from 'iconsax-react';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from "react";
+
+import { MessageQuestion } from "iconsax-react";
 
 type Props = {
   targetDate: Date;
@@ -7,10 +8,10 @@ type Props = {
 
 const Countdown: React.FC<Props> = ({ targetDate }) => {
   const [timeLeft, setTimeLeft] = useState({
-    days: '00',
-    hours: '00',
-    minutes: '00',
-    seconds: '00',
+    days: "00",
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
   });
 
   const [showModal, setShowModal] = useState(false);
@@ -22,16 +23,24 @@ const Countdown: React.FC<Props> = ({ targetDate }) => {
 
       if (distance < 0) {
         setTimeLeft({
-          days: '00',
-          hours: '00',
-          minutes: '00',
-          seconds: '00',
+          days: "00",
+          hours: "00",
+          minutes: "00",
+          seconds: "00",
         });
       } else {
-        const days = String(Math.floor(distance / (1000 * 60 * 60 * 24))).padStart(2, '0');
-        const hours = String(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).padStart(2, '0');
-        const minutes = String(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))).padStart(2, '0');
-        const seconds = String(Math.floor((distance % (1000 * 60)) / 1000)).padStart(2, '0');
+        const days = String(
+          Math.floor(distance / (1000 * 60 * 60 * 24))
+        ).padStart(2, "0");
+        const hours = String(
+          Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        ).padStart(2, "0");
+        const minutes = String(
+          Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        ).padStart(2, "0");
+        const seconds = String(
+          Math.floor((distance % (1000 * 60)) / 1000)
+        ).padStart(2, "0");
 
         setTimeLeft({
           days,
@@ -51,42 +60,58 @@ const Countdown: React.FC<Props> = ({ targetDate }) => {
   return (
     <div className="flex flex-col items-center justify-center md:mt-12 p-4 rounded-3xl bg-white md:bg-transparent md:shadow-none sm:bg-glass-bg sm:backdrop-blur-md sm:shadow-glass-shadow">
       <h1 className="text-xl md:text-3xl font-semibold flex gap-2 justify-center items-center md:mb-4 text-primary font-BeVietnamPro">
-        Countdown
+        Draw Countdown
         {/* <span className='h-4 w-4 text-gray-500 cursor-pointer' onClick={() => setShowModal(!showModal)}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
             <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
           </svg>
         </span> */}
-        <MessageQuestion size={20}  className="text-sm cursor-pointer" onClick={() => setShowModal(!showModal)} />
+        <MessageQuestion
+          size={20}
+          className="text-sm cursor-pointer"
+          onClick={() => setShowModal(!showModal)}
+        />
       </h1>
       <div className="flex space-x-4 text-center">
         <div className="p-2 bg-zinc-200 rounded-lg">
-          <span className="text-3xl font-semibold font-GeistMono text-primary">{timeLeft.days}</span>
+          <span className="text-3xl font-semibold font-GeistMono text-primary">
+            {timeLeft.days}
+          </span>
           <div className="text-sm text-text">Days</div>
         </div>
         <div className="p-2 bg-zinc-200 rounded-lg">
-          <span className="text-3xl font-semibold font-GeistMono text-primary">{timeLeft.hours}</span>
+          <span className="text-3xl font-semibold font-GeistMono text-primary">
+            {timeLeft.hours}
+          </span>
           <div className="text-sm text-text">Hours</div>
         </div>
         <div className="p-2 bg-zinc-200 rounded-lg">
-          <span className="text-3xl font-semibold font-GeistMono text-primary">{timeLeft.minutes}</span>
+          <span className="text-3xl font-semibold font-GeistMono text-primary">
+            {timeLeft.minutes}
+          </span>
           <div className="text-sm text-text">Minutes</div>
         </div>
         <div className="p-2 bg-zinc-200 rounded-lg">
-          <span className="text-3xl font-semibold font-GeistMono text-primary">{timeLeft.seconds}</span>
+          <span className="text-3xl font-semibold font-GeistMono text-primary">
+            {timeLeft.seconds}
+          </span>
           <div className="text-sm text-text">Seconds</div>
         </div>
       </div>
 
-
-
       {showModal && (
         <div className="absolute flex flex-col items-center justify-start mt-12 w-[315px] bg-white rounded-3xl ">
           <div className="text-sidebar py-4 px-1 rounded-lg ">
-            <h2 className="text-xl font-BeVietnamPro font-medium">Contact Information</h2>
-            <p className="mb-2 font-BeVietnamPro text-sm">For more information, please contact</p>
-            <p className='font-CabinetGrotesk font-semibold'>+233 55 511 1333 · +233 20 191 1493</p>
-            <p className='font-CabinetGrotesk'>example@gmail.com</p>
+            <h2 className="text-xl font-BeVietnamPro font-medium">
+              Contact Information
+            </h2>
+            <p className="mb-2 font-BeVietnamPro text-sm">
+              For more information, please contact
+            </p>
+            <p className="font-CabinetGrotesk font-semibold">
+              +233 55 511 1333 · +233 20 191 1493
+            </p>
+            <p className="font-CabinetGrotesk">example@gmail.com</p>
             <button
               className="mt-4 w-full bg-primary text-white px-4 py-2 rounded-full"
               onClick={() => setShowModal(false)}
--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,88 +1,90 @@
-"use client"
+"use client";
 
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-import Head from 'next/head';
-import HeaderFragment from './fragments/Header';
-import ImageCarousel from './fragments/ImageCarousel';
-import PaymentConfirmation from '@/components/confirmPayment';
-import TokenPurchaseForm from '@/components/forms/buy';
-import TripDetailsPage from './fragments/TripDetails';
+import Head from "next/head";
+import HeaderFragment from "./fragments/Header";
+import ImageCarousel from "./fragments/ImageCarousel";
+import PaymentConfirmation from "@/components/confirmPayment";
+import TokenPurchaseForm from "@/components/forms/buy";
+import TripDetailsPage from "./fragments/TripDetails";
 
 type Props = {};
 export interface IPaymentData {
-    status: "success" | "failure",
-    reference: string,
-    checkoutid: string,
+  status: "success" | "failure";
+  reference: string;
+  checkoutid: string;
 }
 
 const Page = (props: Props) => {
-    const [buying, setBuying] = React.useState(false);
-    const [payment, setPayment] = React.useState(false);
-    const [paymentData, setPaymentData] = React.useState<IPaymentData | null>(null)
+  //   const [buying, setBuying] = React.useState(false);
+  //   const [payment, setPayment] = React.useState(false);
+  //   const [paymentData, setPaymentData] = React.useState<IPaymentData | null>(
+  //     null
+  //   );
 
-    useEffect(() => {
-        // Get the current URL's search parameters
-        const urlParams = new URLSearchParams(window.location.search);
-        const action = urlParams.get('action');
+  //   useEffect(() => {
+  //     // Get the current URL's search parameters
+  //     const urlParams = new URLSearchParams(window.location.search);
+  //     const action = urlParams.get("action");
 
+  //     // Check if action is 'purchase' and set buying status accordingly
+  //     if (action === "purchase") {
+  //       setBuying(true);
+  //     }
+  //     if (action === "payment") {
+  //       setPaymentData({
+  //         status: urlParams.get("status") as "success" | "failure",
+  //         reference: urlParams.get("reference") as string,
+  //         checkoutid: urlParams.get("checkoutid") as string,
+  //       });
+  //       setPayment(true);
+  //     }
+  //   }, []); // Empty dependency array ensures this runs once on mount
 
-        // Check if action is 'purchase' and set buying status accordingly
-        if (action === 'purchase') {
-            setBuying(true);
-        }
-        if (action === 'payment') {
-            setPaymentData({
-                status: urlParams.get('status') as "success" | "failure",
-                reference: urlParams.get('reference') as string,
-                checkoutid: urlParams.get('checkoutid') as string,
-            });
-            setPayment(true);
-        }
-    }, []); // Empty dependency array ensures this runs once on mount
+  //   const handleBuyClick = (state: boolean) => {
+  //     //set action to purchase in the URL if state is true, else remove the action parameter
+  //     const urlParams = new URLSearchParams(window.location.search);
+  //     if (state) {
+  //       urlParams.set("action", "purchase");
+  //     } else {
+  //       urlParams.delete("action");
+  //     }
+  //     const newUrl = `${window.location.pathname}?${urlParams.toString()}`;
+  //     window.history.pushState({}, "", newUrl);
+  //     setBuying(state);
+  //   };
 
-    const handleBuyClick = (state: boolean) => {
-        //set action to purchase in the URL if state is true, else remove the action parameter
-        const urlParams = new URLSearchParams(window.location.search);
-        if (state) {
-            urlParams.set('action', 'purchase');
-        } else {
-            urlParams.delete('action');
-        }
-        const newUrl = `${window.location.pathname}?${urlParams.toString()}`;
-        window.history.pushState({}, '', newUrl);
-        setBuying(state);
-    }
+  return (
+    <div className="explore h-screen w-screen">
+      <Head>
+        <title>ROTARY D9104 CAR RAFFLE</title>
+        <meta name="description" content="ROTARY D9104 CAR RAFFLE" />
+        <link rel="icon" href="/assets/logo.png" />
+      </Head>
 
-    return (
-        <div className='explore h-screen w-screen'>
-            <Head>
-                <title>ROTARY D9104 CAR RAFFLE</title>
-                <meta name="description" content="ROTARY D9104 CAR RAFFLE" />
-                <link rel="icon" href="/assets/logo.png" />
-            </Head>
+      <HeaderFragment buying={false} />
 
-            <HeaderFragment buying={buying} />
-
-            <ImageCarousel />
-            {
-                buying ? <TokenPurchaseForm onClose={() => handleBuyClick(false)} /> : (payment && paymentData) ? <PaymentConfirmation {...paymentData} /> : (
-                    <div className="w-full md:w-[750px] md:h-1/3 md:px-16 py-4 absolute bottom-4 md:bottom-6">
-                        <p className='px-4 text-xl font-bold text-[#F3A118] font-GeistMono text-shadow-lg'>
-                            A raffle to fundraise to
-                        </p>
-                        <h1 className='p-4 text-white text-xl md:text-2xl leading-tight text-pretty font-semibold font-VietnamPro text-shadow-md'>
-                            support the construction of 300 Solar Vaccine Shelters in deprived communities across Ghana
-                        </h1>
-                        <TripDetailsPage onBuyClose={() => handleBuyClick(true)} />
-                    </div>
-                )
-            }
-            {
-
-            }
-        </div>
-    );
-}
+      <ImageCarousel />
+      {/* {buying ? (
+        <TokenPurchaseForm onClose={() => handleBuyClick(false)} />
+      ) : payment && paymentData ? (
+        <PaymentConfirmation {...paymentData} />
+      ) : (
+        <></>
+      )} */}
+      <div className="w-full md:w-[750px] md:h-1/3 md:px-16 py-4 absolute bottom-4 md:bottom-6">
+        <p className="px-4 text-xl font-bold text-[#F3A118] font-GeistMono text-shadow-lg">
+          A raffle to fundraise to
+        </p>
+        <h1 className="p-4 text-white text-xl md:text-2xl leading-tight text-pretty font-semibold font-VietnamPro text-shadow-md">
+          support the construction of 300 Solar Vaccine Shelters in deprived
+          communities across Ghana
+        </h1>
+        <TripDetailsPage onBuyClose={() => {}} />
+      </div>
+    </div>
+  );
+};
 
 export default Page;
